abdd0a22c3340d528b69bb9a755ed0055f84d018
George <ubong_george@ymail.com>
2021-02-25 23:51:14 +0100

    Add each method and do block to scrape data to terminal

--- bin/search.rb
+++ bin/search.rb
@@ -5,3 +5,13 @@ designers = Designers.new
 brand = designers.brand_names
 product = designers.description
 price = designers.price
+
+(0...30).each do |index|
+  puts "\n"
+  puts "- - - Men's Designer Sneakers | Search Results: #{index + 1} - - -"
+  puts "\n"
+  puts "Brand: #{brand[index]}"
+  puts "Description: #{product[index].capitalize}"
+  puts "Price: #{price[index]}"
+  puts "\n"
+end
