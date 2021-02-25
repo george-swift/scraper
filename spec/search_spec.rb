92a9a90f7a16d8e511648b06f236ff16309ca376
George <ubong_george@ymail.com>
2021-02-26 00:24:04 +0100

    Add describe block and test cases for #brand_names method

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -1,4 +1,17 @@
 require_relative '../lib/scrape_farfetch'
 
 describe Designers do
+  let(:designer) { Designers.new }
+
+  describe '#brand_names' do
+    context 'when method is called on instance' do
+      it 'returns an array of strings' do
+        expect(designer.brand_names).not_to be nil
+      end
+
+      it 'returns a copy of self with all nil elements removed' do
+        expect(designer.brand_names.include? nil).to be false
+      end
+    end
+  end
 end
