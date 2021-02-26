067ab6c2463cf94cd198e815f75ad42e3bb36c68
George <ubong_george@ymail.com>
2021-02-26 16:57:28 +0100

    Add corrections following linters check

--- bin/search.rb
+++ bin/search.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# rubocop:disable Metrics/BlockLength
 
 require_relative '../lib/scrape_farfetch'
 require 'colorize'
@@ -9,14 +10,14 @@ def start_scrape
   product = designers.description
   price = designers.price
   puts "\n"
-  puts "Hello, window shopper! I am Jarvis"
+  puts 'Hello, window shopper! I am Jarvis'
   puts "\n"
   sleep(1)
-  puts "How many collections do you want to check out today?"
+  puts 'How many collections do you want to check out today?'
   puts "\n"
-  puts "⚠️  You can only say in numbers".red
+  puts '⚠️  You can only say in numbers'.red
   puts "\n"
-  collections = gets.chomp.to_i 
+  collections = gets.chomp.to_i
   (0...collections).each do |index|
     puts "- - - Men's Designer Sneakers | Search Results: #{index + 1} - - -".white
     puts "\n"
@@ -26,5 +27,6 @@ def start_scrape
     puts "\n"
   end
 end
+# rubocop:disable Metrics/BlockLength
 
 start_scrape
