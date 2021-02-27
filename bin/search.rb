f09c425a239d07389cd5e1bf9727b10229df20e4
George <ubong_george@ymail.com>
2021-02-27 13:14:57 +0100

    Refactor to eliminate assignment branch size error associated with method

--- bin/search.rb
+++ bin/search.rb
@@ -3,30 +3,18 @@
 require_relative '../lib/scrape_farfetch'
 require 'colorize'
 
-# rubocop:disable Metrics/MethodLength, Metrics/AbcSize
-def scrape
-  designers = Designers.new
-  brand = designers.brand_names
-  product = designers.description
-  price = designers.price
-  puts "\n"
-  puts 'Hello, window shopper! I am Jarvis'
-  puts "\n"
-  sleep(1)
-  puts 'How many collections do you want to check out today?'
-  puts "\n"
-  puts '⚠️  You can only say in numbers'.red
-  puts "\n"
-  collections = gets.chomp.to_i
-  (0...collections).each do |index|
-    puts "- - - Men's Designer Sneakers | Search Results: #{index + 1} - - -".white
-    puts "\n"
-    puts "Brand: #{brand[index]}".green
-    puts "Description: #{product[index].capitalize}".green
-    puts "Price: #{price[index]}".yellow
-    puts "\n"
-  end
+designers = Designers.new
+brand = designers.brand_names
+product = designers.description
+price = designers.price
+puts "\nHello, window shopper! 👋🏽 I am Jarvis \n\n"
+sleep(1)
+puts "How many collections do you want to check out today? \n"
+puts "⚠️  You can only say in numbers \n".red
+collections = gets.chomp.to_i
+(0...collections).each do |index|
+  puts "- - - Men's Designer Sneakers | Search Results: #{index + 1} - - -".white
+  puts "\nBrand: #{brand[index]}".green
+  puts "Description: #{product[index].capitalize}".green
+  puts "Price: #{price[index]}\n".yellow
 end
-# rubocop:enable Metrics/MethodLength, Metrics/AbcSize
-
-scrape
